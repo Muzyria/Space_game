4458a9f9b79e2dad2c54e68e425a29817f48fd65
Muzyria <fila090580@gmail.com>
2023-03-08 23:34:11 +0200

    new

--- scores.py
+++ scores.py
@@ -10,6 +10,7 @@ class Scores:
         self.text_color = (139, 195, 74)
         self.font = pygame.font.SysFont(None, 36)
         self.image_score()
+        self.image_high_score()
 
 
     def image_score(self):
@@ -18,5 +19,13 @@ class Scores:
         self.score_rect.right = self.screen_rect.right - 40
         self.score_rect.top = 20
 
+    def image_high_score(self):
+        self.high_score_image = self.font.render(str(self.stats.high_score), True, self.text_color, (0, 0, 0))
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.screen_rect.top + 20
+
+
     def show_score(self):
         self.screen.blit(self.score_img, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
--- stats.py
+++ stats.py
@@ -4,6 +4,8 @@ class Stats:
         """init statistics"""
         self.reset_stats()
         self.run_game = True
+        with open('highscore.txt', 'r') as f:
+            self.high_score = int(f.readline())
 
     def reset_stats(self):
         self.guns_left = 2
